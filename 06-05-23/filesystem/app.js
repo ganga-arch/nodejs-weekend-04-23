c21065f1be5c85507db96ac4000de8de0fa86c9d
salman3009 <salman444g@gmail.com>
2023-05-06 22:03:58 +0530

    code

--- 06-05-23/filesystem/app.js
+++ 06-05-23/filesystem/app.js
@@ -1,13 +1,28 @@
 const fs = require('fs');
 
 console.log("line 1");
+
+const readFile =()=>{
 //async 
-fs.readFile('input.txt',(err,data)=>{
-     if(err){
-        console.log("some error occur");
-        return;
-     }
-     //buffer to unicode character
-     console.log(data.toString());
+fs.readFile('./data/input.txt',(err,data)=>{
+    if(err){
+       console.log("some error occur");
+       return;
+    }
+    //buffer to unicode character
+    console.log(data.toString());
 })
+}
+// readFile();
+
+const writeFile=()=>{
+    const input = "Welcome to javascript and angular";
+     fs.writeFile('./data/read.txt',input,(err)=>{
+        if(err){
+            return console.log(err);
+        }
+        console.log("file created successfully");
+     })
+}
+writeFile();
 console.log("line 12");
