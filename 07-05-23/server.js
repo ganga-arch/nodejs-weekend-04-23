29ead7513d8a55208f8e2314b0a74234e716b82f
salman3009 <salman444g@gmail.com>
2023-05-07 09:21:33 +0530

    code

--- 07-05-23/server.js
+++ 07-05-23/server.js
@@ -3,8 +3,18 @@ const http = require('http');
 const server = http.createServer((req,res)=>{
   //req object anything that is coming from frontend
   //res we are sending data to frontend
-  res.writeHead(201,{'Content-Type':'text/html'});
-  res.end('<h1>welcome to html</h1>');
+
+
+// res.writeHead(201,{'Content-Type':'text/html'});
+// res.end("<h1>Hello world</h1>");
+
+//   res.writeHead(201,{'Content-Type':'application/json'});
+//   res.end('{"fullName":"akash","type":"chennai"}');
+
+res.writeHead(201,{'Content-Type':'text/plain'});
+res.end('file is reading');
+
+
 });
 
 server.listen(3000,()=>{
